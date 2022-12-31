d298e4fef34718c906b764dd7e5ff9d9c4eda6a6
Jack Maguire <maguirj4@gene.com>
2022-12-31 07:13:05 -0600

    big refactor for incoming and outgoing edges

--- src/jax_xenet/xenet.py
+++ src/jax_xenet/xenet.py
@@ -31,66 +31,78 @@ def mpnn(edges, edge_features, node_features):
 
     key = jax.random.PRNGKey(0)
     num_nodes = node_features.shape[0]
-    num_edge_features = edge_features.shape[-1]
     num_node_features = node_features.shape[-1]
+    num_edges = edge_features.shape[0]
+    num_edge_features = edge_features.shape[-1]
     node_features_updated = node_features
 
+    Fout = num_node_features
+
     # Define MLP weights and biases for edges
-    weights_edge = jax.random.normal( key=key, shape=(num_edge_features, num_node_features) )
-    biases_edge = jax.random.normal( key=key, shape=(num_node_features,) )
+    message_input_size = 2*num_node_features + 2*num_edge_features
+    stack_sizes = [ 64, 64 ]
+    def stack_input_size( i ):
+        if i == 0: return message_input_size
+        else:      return stack_sizes[ i-1 ]
+    n_stack_convs = len(stack_sizes)
+    
+    weights_edge = [ jax.random.normal( key=key, shape=( stack_input_size(i), stack_sizes[i]) ) for i in range(len(stack_sizes)) ] 
+    biases_edge = [ jax.random.normal( key=key, shape=( stack_sizes[i], ) ) for i in range(len(stack_sizes)) ] 
+
     # Define MLP weights and biases for nodes
-    weights_node = jax.random.normal( key=key, shape=(num_node_features * 2, num_node_features) )
-    biases_node = jax.random.normal( key=key, shape=(num_node_features,) )
+    weights_node = jax.random.normal( key=key, shape=(num_node_features + (stack_sizes[-1]*2), Fout) )
+    biases_node = jax.random.normal( key=key, shape=(Fout,) )
     
     reverse_edge_error_flag = np.ones((1,))
 
-    # Propagation step
-    num_propagate_steps = 2
-    for _ in range(num_propagate_steps):
-        # Initialize messages with zeros
-        messages = np.zeros((num_nodes, num_node_features))
-        # Iterate over edges
-        for edge_idx in range(edges.shape[0]):
-            src_node, dest_node = edges[edge_idx]
+    # Initialize messages with zeros
+    #messages = np.zeros((num_nodes, Fout))
+    incoming_stacks = np.zeros((num_nodes, stack_sizes[-1]))
+    outgoing_stacks = np.zeros((num_nodes, stack_sizes[-1]))
 
-            # Check if there is a reverse edge
-            reverse_edge_mask = (edges[:, 0] == dest_node) & (edges[:, 1] == src_node)
-            rev_edge_idx = np.argmax(reverse_edge_mask)
+    # Iterate over edges
+    for edge_idx in range(edges.shape[0]):
+        src_node, dest_node = edges[edge_idx]
 
-            if debug_mode:
-                _dest_node, _src_node = edges[rev_edge_idx]
-                reverse_edge_error_flag *= np.equal( dest_node, _dest_node )
-                reverse_edge_error_flag *= np.equal( src_node, _src_node )
+        # Check if there is a reverse edge
+        reverse_edge_mask = (edges[:, 0] == dest_node) & (edges[:, 1] == src_node)
+        rev_edge_idx = np.argmax(reverse_edge_mask)
 
-            x_i = node_features[ src_node ]
-            x_j = node_features[ dest_node ]
-            e_ij = edge_features[ edge_idx ]
-            e_ji = edge_features[ rev_edge_idx ]
+        if debug_mode:
+            _dest_node, _src_node = edges[rev_edge_idx]
+            reverse_edge_error_flag *= np.equal( dest_node, _dest_node )
+            reverse_edge_error_flag *= np.equal( src_node, _src_node )
 
-            message_input = np.concatenate(
-                [ x_i, x_j, e_ij, e_ji ],
-                axis=-1
-            )
-            #if np.any(reverse_edge_mask):
-            # Compute message for destination node using MLP
-            message = nn.relu(
-                    np.dot(edge_features[edge_idx], weights_edge) + biases_edge
-                )
-            message = np.dot(message, node_features[src_node])
-            # Accumulate message for destination node
-            #
-            #messages[dest_node] += message
-            # TypeError: '<class 'jax.interpreters.partial_eval.DynamicJaxprTracer'>' object does not support item assignment. JAX arrays are immutable. Instead of ``x[idx] = y``, use ``x = x.at[idx].set(y)`` or another .at[] method: https://jax.readthedocs.io/en/latest/_autosummary/jax.numpy.ndarray.at.html
-            messages.at[dest_node].set( messages[dest_node] + message )
+        x_i = node_features[ src_node ]
+        x_j = node_features[ dest_node ]
+        e_ij = edge_features[ edge_idx ]
+        e_ji = edge_features[ rev_edge_idx ]
 
-        # Aggregate messages and node features
-        node_features_updated = np.concatenate(
-            [node_features_updated, messages], axis=-1
+        stack = np.concatenate(
+            [ x_i, x_j, e_ij, e_ji ],
+            axis=-1
         )
-        # Apply MLP to update node features
-        node_features_updated = nn.relu(
-            np.dot(node_features_updated, weights_node) + biases_node
-        )
+
+        # Compute message for destination node using MLP
+        for i in range( n_stack_convs ):
+            stack = np.dot(stack, weights_edge[i]) + biases_edge[i]
+            stack = nn.relu( stack )
+
+        # TODO attention
+
+        # Accumulate messages for nodes
+        incoming_stacks.at[dest_node].set( incoming_stacks[dest_node] + stack )
+        outgoing_stacks.at[src_node].set( outgoing_stacks[src_node] + stack )
+
+    # Aggregate messages and node features
+    node_features_updated = np.concatenate(
+        [node_features, incoming_stacks, outgoing_stacks],
+        axis=-1
+    )
+    # Apply MLP to update node features
+    node_features_updated = nn.relu(
+        np.dot(node_features_updated, weights_node) + biases_node
+    )
     
     # flip all flags such that 0 means "pass"
     reverse_edge_error_flag = 1 - reverse_edge_error_flag
@@ -104,13 +116,11 @@ def mpnn(edges, edge_features, node_features):
 mpnn_jit = jit(mpnn)#, static_argnames=['num_propagate_steps'])
 
 def test():
-    # Test the function with some
     #edges = jnp.array([[0, 1], [1, 2], [2, 3], [3, 0]])
     edges = jnp.array([[0, 1], [1, 2], [2, 3], [3, 0], [1, 0], [2, 1], [3, 2], [0, 3] ])
     num_nodes = 4
     num_node_features = 3
     num_edge_features = 5
-    #num_propagate_steps = 2
 
     key = jax.random.PRNGKey(0)
     edge_features = jax.random.uniform( key, shape=(edges.shape[0], num_edge_features) )
